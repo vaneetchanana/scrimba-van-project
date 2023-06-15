a8ab13a985320be3c4255cb4629a9da208465cbc
Vaneet Chanana <vaneetchanana98@gmail.com>
2023-06-15 20:14:13 +0530

    added the functionality to catch the thrown error with useRouterError hook

--- components/Error.jsx
+++ components/Error.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useRouteError } from 'react-router-dom'
 
 function Error() {
-  return (
-    <h1>An error occurred!</h1>
-  )
+    const error = useRouteError()
+    console.log(error)
+    return (
+        <>
+            <h1>Error: {error.message}</h1>
+            <pre>{error.status} - {error.statusText}</pre>
+        </>
+    )
 }
 
 export default Error
